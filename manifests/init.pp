648320b76dd1e36279b5ff8df0d94f64756feeff
Gabriel Nadler <Gabriel.Nadler@swisscom.com>
2015-05-21 11:50:28 +0200

    Maybe this works

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
 # This is a placeholder class.
-class bashconfiguration {
-  anchor { 'Hello_World': }
+class bashconfiguration($user='tzhnaga1') {
+  file {"/home/${user}/.bash_profile":
+    ensure => present,
+    source => "puppet:///modules/userconfig/bashrc",
+  }
+  file {"/home/${user}/.inputrc":
+    ensure => present,
+    source => "puppet:///modules/userconfig/inputrc",
+  }
 }
